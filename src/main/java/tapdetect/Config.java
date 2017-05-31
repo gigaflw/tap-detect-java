8d4fb3bb486e39fb160a403e4835cd58383e9396
zhouben <a_gigantic_flower@yeah.net>
2017-05-31 15:49:21 +0800

    decrease default para

--- src/main/java/tapdetect/Config.java
+++ src/main/java/tapdetect/Config.java
@@ -2,7 +2,7 @@
 * @Author: zhouben
 * @Date:   2017-05-10 14:37:27
 * @Last Modified by:   zhouben
-* @Last Modified time: 2017-05-30 21:12:18
+* @Last Modified time: 2017-05-31 15:48:39
 */
 
 package tapdetect;
@@ -13,9 +13,9 @@ public class Config {
     public static Scalar[] FINGER_COLOR_RANGE_SM = {new Scalar(0, 150, 100), new Scalar(255, 160, 130)};
     public static Scalar[] FINGER_COLOR_RANGE_LG = {new Scalar(0, 145, 100), new Scalar(255, 165, 140)};
 
-    public static int IM_HEIGHT = 500;
-    public static int TAP_THRESHOLD_ROW = 320;
-    public static int HAND_AREA_MIN = 1200;
+    public static int IM_HEIGHT = 250;
+    public static int TAP_THRESHOLD_ROW = 160;
+    public static int HAND_AREA_MIN = 300;
     public static int FINGER_TIP_STEP = 10;
     public static int FINGER_TIP_WIDTH = 30;
 }
